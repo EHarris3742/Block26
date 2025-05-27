ffdf032c588c6d15f49713854a7fb2dd1654b711
Eazywrlddd <163927038+Eazywrlddd@users.noreply.github.com>
2025-05-27 10:33:51 -0500

    initial com

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ContactList from "./components/ContactList";
+import './App.css'
+
+export default function App() {
+  
+
+  return (
+    <>
+      <ContactList />
+        
+    </>
+  )
+}
+
+
